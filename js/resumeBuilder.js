7832e3c71168a89e16a86db4e19dba18928955c6
Brandon Wong <brandon.wong@seagate.com>
2016-09-14 17:07:24 -0500

    edited index and resume builder. Completed JavaScript basics up until Problem set 1. Onto Flow control next.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,88 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append(["Brandon Wong"]);
+var bio =
+{
+	"name": "Brandon Wong",
+	"role": "Software Engineer",
+	"contacts": {
+		"mobile": "469-682-6913",
+		"e-mail": "b.lh.wong@utexas.edu",
+		"github": "blhwong",
+		"linkedin": "blhwong"
+	},
+	"pictureURL": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAj6AAAAJGUyZGU0ZTk4LWM0M2QtNDI3MS05MGQxLTdkOTgyYTBkNzJlZA.jpg",
+	"welcomeMessage": "Seeking a software engineering position where my passion in programming is utilized",
+	"skills": ["C", "C++", "Java", "JavaScript", "Python", "git", "bash"]
+};
+
+var work =
+{
+	"jobs": [{
+		"employer": "Seagate Technology",
+		"title": "Field Applications Engineer",
+		"location": "Round Rock, TX",
+		"dates": "01/2016 - Present"
+	}, {
+		"employer": "Student Loan Assistance LLC",
+		"title": "Information Technology Analyst",
+		"location": "Addison, TX",
+		"dates": "01/2015 - 09/2015"
+	}, {
+		"employer": "Office of Student Financial Services",
+		"title": "Peer Advisor",
+		"location": "Austin, TX",
+		"dates": "08/2013 - 08/2014"
+	}]
+};
+
+var projects =
+{
+	"projects": [{
+		"title": "Federal Student Loan Puller",
+		"dates": "08/2015 - 09/2015",
+		"description": "• Led design of proof of concept system using Python scripts to navigate webpages and download client data with client usernames and passwords and explored potential for developing a portal and data parser\n•Iterated URL to navigate through pages",
+		"images": []
+	}, {
+		"title": "Color Hex Android Application",
+		"dates": "09/2014",
+		"description": "• Designed color mixing mobile application to output both a color and a HTML-friendly hexadecimal code based on independent adjustments to the alpha, red, green, and blue channels on an XML coded interface\n• Alpha, red, green, blue channels each have a two digit hexadecimal range.",
+		"images": []
+	}, {
+		"title": "Behavioral Level Design of LC-3 Architecture",
+		"dates": "01/2014 - 05/2014",
+		"description": "• Designed sections of microarchitecture to accommodate interrupt and exception handling, one level virtual address translation, and an improved pipelined architecture that tripled processing efficiency\n• Programmed assembler to translate LC-3 assembly language to machine language",
+		"images": []
+	}, {
+		"title": "LED Cube Equalizer",
+		"dates": "09/2013 - 12/2013",
+		"description": "• Soldered 125 light emitting diodes to create a 5x5x5 cube equalizer\n• Designed a printed circuit board to individually multiplex 125 LEDs.\n• Utilized a sound jack to retrieve analog to digital signals of music\n• Programmed pattern designs on the cube using digital signals from music",
+		"images": []
+	}, {
+		"title": "Investigation for Improved Multiplexer in Ultrasonic Flow Meter",
+		"dates": "08/2013 - 05/2014",
+		"description": "• Group designed pulse generator, DC-DC converter, multiplexer, transducer modules\n• Replacement of company chip to a proposed improved chip with higher noise isolation\n• Investigated the signal to noise ratio of chips by measuring under an oscilloscope\n• Communication between team members and sponsors to overcome technical difficulties",
+		"images": []
+	}, {
+		"title": "Tabbed List Android Application",
+		"dates": "12/2014",
+		"description": "• Designed interface using relative layout with XML code\n• Interfaced three tabs to swipe between lists\n• Lists accommodates insertion and deletion\n• Investigated obtaining and saving lists into local storage\n• Considered code optimization for list manipulations",
+		"images": []
+	}, {
+		"title": "Client Database using MySQL",
+		"dates": "02/2015 - 09/2015",
+		"description": "• Created sixteen column database\n• Wrote scripts to import comma database files to tables\n• Managed and analyzed client payment information\n• Generate reports under any given conditions",
+		"images": []
+	}]
+};
+
+var education =
+{
+	"schools": [{
+		"name": "The University of Texas at Austin",
+		"location": "Austin, TX",
+		"graduation date": 2014,
+		"majors": "Electrical Computer Engineering"
+	}]
+};
